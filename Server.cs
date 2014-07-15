1ee112b30484375e5968c90ca780eaa2fda1b974
Fadi Hanna Al-Kass <FadiEliya1@users.noreply.github.com>
2014-07-15 00:48:49 -0400

    Update Server.cs

--- Server.cs
+++ Server.cs
@@ -8,14 +8,16 @@ namespace Bluetooth_Chat_server
 		public static void Main (string[] args)
 		{
 
-			const string COM_PORT = "COM#";	/* replace the # with the COM Port number corresponding to your
-									       established virtual COM Port number; e.g., COM1, COM12, etc... */
+			/* replace the # with the COM Port number corresponding to your
+			   established virtual COM Port number; e.g., COM1, COM12, etc... */
+			const string COM_PORT = "COM#";
 
 			const int BAUD_RATE = 9600;	/* 9600 MHz for baud rate, we'll discuss this value later */
 
 
-			SerialPort sp = new SerialPort (COM_PORT, BAUD_RATE);	/* Establish an instantiation of SerialPort class that
-																	   reads off Com Port @COM_PORT at @BAUD_RATE MHz */
+			/* Establish an instantiation of SerialPort class that
+			  reads off Com Port @COM_PORT at @BAUD_RATE MHz */
+			SerialPort sp = new SerialPort (COM_PORT, BAUD_RATE);							   
 
 			sp.Open ();		/* Open the desired COM Port
 							 * Note: The program will crash at this line if you specify the wrong COM Port or Baud Rate
